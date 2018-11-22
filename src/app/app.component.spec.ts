1b72ee147df06d48b4c9a3075ae0d5e7681b79d5
saurabh <s.e.rai@accenture.com>
2018-11-22 19:47:36 +0530

    build 34

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,19 +1,51 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed, async ,fakeAsync, tick, getTestBed} from '@angular/core/testing';
+// import { RouterTestingModule } from '@angular/router/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+// import { HomeComponent } from './home/home.component';
+// import { LoginComponent } from './login/login.component';
+// import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+// const routes: Routes = [
+//   { path: 'edit', component: HomeComponent },
+//   { path: 'login', component: LoginComponent },
+//   { path: '',	component: HomeComponent},
+//   { path: '**',	component: PageNotFoundComponent},
+// ];
 
 describe('AppComponent', () => {
+ 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+       
+        RouterModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        // LoginComponent,
+        // HomeComponent,
+        // PageNotFoundComponent
       ],
     }).compileComponents();
+    
   }));
 
+  // it('should be able to navigate to `/`',
+  //   fakeAsync(() => {
+  //     const injector = getTestBed()
+  //     const router = injector.get(Router);
+  //     const fixture = TestBed.createComponent(AppComponent);
+  //     fixture.detectChanges();
+  //     // initial navigation
+  //     router.navigate(['/edit'])
+  //       .then(() => {
+  //         expect(router.url).toEqual('');
+  //       });
+  //     }));
+
+      
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
@@ -23,13 +55,12 @@ describe('AppComponent', () => {
   it(`should have as title 'P2c-webapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('P2c-webapp');
+    expect(app.title).toEqual('P2cwebapp');
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to P2c-webapp!');
-  });
+
 });
+
+
+
+
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -22,4 +22,12 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('It should have iframe', () => {
+    const fixture = TestBed.createComponent(HomeComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('iframe').textContent).toContain('https://www.Fiat.it');
+  });
+  
 });
